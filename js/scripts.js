d40857a576a7fb7be9b95337486dad47935621c5
MikeGoz <gozdz@onet.pl>
2017-04-20 15:00:08 +0200

    Task_8_4

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imię');
+// alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+// Single line comment
+
+/* 
+Multi
+-line 
+comments 
+*/
